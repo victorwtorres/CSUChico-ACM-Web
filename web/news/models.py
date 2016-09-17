f2db98b740d6503ec5c271ce709941237edc3c6c
Ubuntu <ubuntu@ip-172-31-3-127.us-west-1.compute.internal>
2016-09-17 03:23:20 +0000

    Updates acmcsuchico.com

--- web/news/models.py
+++ web/news/models.py
@@ -2,21 +2,27 @@ from django.db import models
 from django.core.urlresolvers import reverse
 
 class Post(models.Model):
-	title = models.CharField(max_length=255)
-	slug = models.SlugField(max_length=255, unique=True)
-	created = models.DateTimeField()
-	content = models.TextField()
-	published = models.BooleanField(default=False)
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True)
+    created = models.DateTimeField()
+    content = models.TextField()
+    published = models.BooleanField(default=False)
         
-	class Meta:
-        	ordering = ['-created']
+    class Meta:
+        ordering = ['-created']
+
+    def __str__(self):
+        return str(self.title)
 
 class Link(models.Model):
-        title = models.CharField(max_length=255)
-        slug = models.SlugField(max_length=255, unique=True)
-        created = models.DateTimeField()
-        content = models.TextField()
-        published = models.BooleanField(default=False)
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True)
+    created = models.DateTimeField()
+    content = models.TextField()
+    published = models.BooleanField(default=False)
 
-        class Meta:
-                ordering = ['-created']
+    class Meta:
+        ordering = ['-created']
+
+    def __str__(self):
+        return str(self.title)
